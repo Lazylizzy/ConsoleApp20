01bbe586f243e5fb57a66c5be773278f1f188e62
lazylizzy <petrovalizpetrova@yandex.ru>
2021-11-09 22:22:14 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    // Создадим делегат
+    delegate double DoubleOperation(double i);
+
+    class Program
+    {
+        // Организуем ряд методов
+        static double Dl(double r)
+        {
+            return 2*Math.PI*r;
+        }
+
+        static double S(double r)
+        {
+            return Math.PI * Math.Pow(r, 2);
+        }
+
+        static double V(double r)
+        {
+            return (4/3)*Math.PI * Math.Pow(r, 3);
+        }
+
+        static void Main()
+        {
+            // Сконструируем делегат
+            DoubleOperation r1 = new DoubleOperation(Dl);
+
+            double result = r1(5);
+            Console.WriteLine("Длина: " + result);
+
+            // Изменим ссылку на метод
+            r1 = new DoubleOperation(S);
+            result = r1(5);
+            Console.WriteLine("Площадь: " + result);
+            r1 = new DoubleOperation(V);
+            result = r1(5);
+            Console.WriteLine("Объем: " + result);
+
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
